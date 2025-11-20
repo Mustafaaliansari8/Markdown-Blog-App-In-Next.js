7cdcead2fb642864f3748a79ed937c24e08f91d1
Mustafaaliansari <imustafaali04@gmail.com>
2025-11-20 22:01:55 +0530

    .

--- components/Navbar.js
+++ components/Navbar.js
@@ -89,7 +89,7 @@ const Navbar = () => {
 
           <div className="md:hidden flex items-center gap-4">
             <AnimatedThemeToggler />
-            <Sheet modal={false}>
+            <Sheet>
               <SheetTrigger>
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
